3326953c1d30d754ad70174b39623ac0fdaf2cea
s1f102100704 <s1f102100704@iniad.org>
2025-04-19 15:35:08 +0900

    postをキャッシュで処理

--- backend/app/queries/posts_query.rb
+++ backend/app/queries/posts_query.rb
@@ -13,16 +13,5 @@ class PostsQuery
   def self.fetch_by_discussion_thread(discussion_thread_id)
     Post.includes(:votes)
         .where(discussion_thread_id: discussion_thread_id)
-        .map do |post|
-          {
-            id: post.id,
-            content: post.content,
-            discussion_thread_id: post.discussion_thread_id,
-            gender: post.gender,
-            votes: post.votes.map { |vote| { id: vote.id, gender: vote.gender, vote_type: vote.vote_type } },
-            created_at: post.created_at,
-            image_key: post.image_key,
-          }
-        end
   end
 end
---
+++ backend/app/serializers/vote_serializer.rb
@@ -0,0 +1,3 @@
+class VoteSerializer < ActiveModel::Serializer
+  attributes :id, :gender, :vote_type
+end
