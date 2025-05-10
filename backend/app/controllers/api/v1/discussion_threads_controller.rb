56d6cf51f675618fbda081e2867ce69a6315ebcd
1F10210096 <s1f102100962@iniad.org>
2025-05-11 01:20:26 +0900

    なにこれ

--- backend/app/controllers/api/v1/discussion_threads_controller.rb
+++ backend/app/controllers/api/v1/discussion_threads_controller.rb
@@ -1,33 +1,32 @@
 module Api
-    module V1
-      class DiscussionThreadsController < ApplicationController
-        def index
-        end
+  module V1
+    class DiscussionThreadsController < ApplicationController
+      def show
+        thread = DiscussionThread.find_by_id!(params[:id])
+        render_json_response(thread)
+      rescue ActiveRecord::RecordNotFound
+        render_json_response({ error: "Thread not found" }, status: :not_found)
+      end
 
-        def show
-          thread = DiscussionThread.find_by_id!(params[:id])
-          render_json_response(thread)
-        end
-  
-        def create
-          thread = DiscussionThread.create_with_post(thread_params, post_params,@current_user.id)
-
-          if thread.persisted?
-            render_json_response({ id: thread.id, thread_title: thread.thread_title,created_at:thread.created_at }, status: :created)
-          else
-            render_json_response({ errors: thread.errors.full_messages }, status: :unprocessable_entity)
-          end
-        end
-  
-        private
-  
-        def thread_params
-          params.require(:discussion_thread).permit(:thread_title,:created_at)
-        end
-
-        def post_params
-          params.require(:post).permit(:content, :gender, :user_id,:image_url)
+      def create
+        thread = DiscussionThread.create_with_post(thread_params, post_params,@current_user.id)
+      
+        if thread.persisted?
+          render_json_response({ id: thread.id, thread_title: thread.thread_title,created_at:thread.created_at,image_key:thread.image_key }, status: :created)
+        else
+          render_json_response({ errors: thread.errors.full_messages }, status: :unprocessable_entity)
         end
       end
+
+      private
+
+      def thread_params
+        params.require(:discussion_thread).permit(:thread_title,:created_at, :image_key)
+      end
+
+      def post_params
+        params.require(:post).permit(:content, :gender, :image_key)
+      end
     end
+  end
 end
