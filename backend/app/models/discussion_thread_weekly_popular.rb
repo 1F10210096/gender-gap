ba54db63a444e0f9e43c0e4a9b5d9bfd0cd3bb00
1F10210096 <s1f102100962@iniad.org>
2025-05-11 12:04:29 +0900

    命名変更

--- backend/app/models/discussion_thread_weekly_popular.rb
+++ backend/app/models/discussion_thread_weekly_popular.rb
@@ -11,18 +11,18 @@ class DiscussionThreadWeeklyPopular
   end
 
   def call
-    popular_threads_cache
+    weekly_popular_threads_cache
   end
 
   private
 
-  def popular_threads_cache
+  def weekly_popular_threads_cache
     Rails.cache.fetch("popular_threads", expire_in: Rails.application.config.cache_expiry[:popular_weekly_threads]) do
-      thread_popular_with_votes
+      weekly_thread_popular_with_votes
     end
   end
 
-  def thread_popular_with_votes
+  def weekly_thread_popular_with_votes
     query.weekPopular.map do |thread|
       summarizer.call(thread: thread)
     end
