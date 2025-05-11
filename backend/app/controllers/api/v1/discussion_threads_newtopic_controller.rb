59edf5e7aa7c780c24a0f926fd92fa95e264049f
1F10210096 <s1f102100962@iniad.org>
2025-05-11 14:12:02 +0900

    完了

--- backend/app/controllers/api/v1/discussion_threads_newtopic_controller.rb
+++
@@ -1,9 +0,0 @@
-module Api
-    module V1
-        class DiscussionThreadsNewtopicController < DiscussionThreadsController
-            def index
-                render_json_response(DiscussionThreadRecent.call)
-            end
-        end
-    end
-end
--- backend/app/controllers/api/v1/discussion_threads_week_populartopic_controller.rb
+++
@@ -1,9 +0,0 @@
-module Api
-    module V1
-        class DiscussionThreadsWeekPopulartopicController < ApplicationController
-            def index
-                render_json_response(DiscussionThreadWeeklyPopular.call)
-            end
-        end
-    end
-end
